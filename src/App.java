eecbf68358b27676c35c17b2ef983582ab8fc361
Anthony <nitinbhagatxyz2@gmail.com>
2024-11-09 03:31:50 +0530

    fixed

--- src/App.java
+++ src/App.java
@@ -5,10 +5,14 @@ public class App {
         int boardWidth = 360;
         int boardHeight = 640;
         JFrame frame = new JFrame("Flappy Game");
-        frame.setVisible(true);
         frame.setSize(boardWidth, boardHeight);
         frame.setLocationRelativeTo(null);
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        FlappyBird flappy_bird = new FlappyBird();
+        frame.add(flappy_bird);
+        frame.pack();
+        flappy_bird.requestFocus();
+        frame.setVisible(true);
     }
 }
